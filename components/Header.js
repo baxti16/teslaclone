2465931681bafa7f909f4c6ebcf0010e8d402b65
baxti16 <96297516+baxti16@users.noreply.github.com>
2022-06-08 00:31:33 +0500

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+import Reveal from 'react-reveal/Reveal'
+
+const Header = () => {
+  return (
+    <section className={styles.section}>
+      <Reveal effect="fadeInUp">
+      <div className={styles.textsArea}>
+        <h3>Model 3</h3>
+        <p>
+          Order Online for
+          <span className={styles.touchless}>Touchles delivery</span>
+        </p>
+      </div>
+      </Reveal>
+      <Reveal effect="fadeInUp2">
+      <div className={styles.buttons}>
+        <div className={styles.btnMain}>
+          <a href="#" className={styles.btn}>
+            custom order
+          </a>
+          <a href="#" className={styles.btn}>
+            exisiting inventory
+          </a>
+        </div>
+      <img
+        src="/img_files/down-arrow.svg"
+        className={styles.downIcon}
+        alt="down icon"
+      />
+      </div>
+      </Reveal>
+    </section>
+  );
+};
+
+export default Header;
